b0c054ffa2c8aeed370d152a8f6b878071cd056b
Navdeep18101990 <169003804+Navdeep18101990@users.noreply.github.com>
2024-07-28 20:18:30 -0400

    Add files via upload

---
+++ AddController.java
@@ -0,0 +1,29 @@
+package com.navi;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AddController 
+{
+	
+	/*@RequestMapping("/add")
+	public void add()
+	{
+		System.out.println("Hii");
+	}*/
+	
+	/*@RequestMapping("/add")
+	public String add()
+	{
+		System.out.println("Hii");
+		return "display.jsp";
+	}*/
+	
+	@RequestMapping("/add")
+	public String add()
+	{
+		int i=Integer.parseInt(request.)
+	}
+
+}
